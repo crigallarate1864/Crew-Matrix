ff7d4a20225487e53c293c6a87c54fc4a53355ad
crigallarate1864 <adriano.luongo@crigallarate.org>
2026-08-09 19:33:01 +0200

    Add files via upload

---
+++ portal/js/config.js
@@ -0,0 +1,11 @@
+
+export const ATLAS_SERVER_URL =
+  'https://script.google.com/macros/s/AKfycbzQv_9pxwVhbjLJuKysh0a52G5BBaGxzKuD38ky4Uigr3YQpG09yBr2pdIrz9gmUnYG/exec';
+
+export const SESSION_KEY='atlas-118-release-1-session';
+
+export const PROFILE_ROUTES=Object.freeze({
+  ADMIN:'admin.html',
+  RO:'ro.html',
+  VOLUNTEER:'volontari.html'
+});
